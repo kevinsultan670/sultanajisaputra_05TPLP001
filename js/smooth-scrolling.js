b19e92318cf654ffd20b928c259f58574e16f40f
kevinsultan670 <127640177+kevinsultan670@users.noreply.github.com>
2024-11-02 21:32:07 +0700

    Add files via upload

---
+++ js/smooth-scrolling.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll("a[href^='#']");
+
+  for (const link of links) {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+        });
+      }
+    });
+  }
+});
